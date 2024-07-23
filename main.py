ad1f6c1042b15a8aadea2b277f018094d4022191
TLA-FMC-FEB <162482058+TLA-FMC-FEB@users.noreply.github.com>
2024-07-23 02:08:08 +0000

    Reback end

--- main.py
+++ main.py
@@ -6,7 +6,24 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+    
+@app.route('/about')
+def about():
+    return render_template("about-us.html")
 
+@app.route('/events')
+def events():
+    return render_template("events.html")
+
+@app.route('/learn')
+def learn():
+    return render_template("learn.html")
+
+@app.route('/research')
+def research():
+    return render_template("research.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+
